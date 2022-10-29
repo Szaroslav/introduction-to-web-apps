7cc65cdba8911a35b769311b976b88de582cb9b8
Jakub <jakub.szaredko@gmail.com>
2022-10-29 15:40:48 +0200

    Create exercise 13, update exercise 6

--- lab2/ex6/app.js
+++
@@ -1,6 +0,0 @@
-addEventListener('DOMContentLoaded', () => {
-    document.querySelector('.nav-button').addEventListener('click', () => {
-        document.querySelector('.nav-button').classList.toggle('clicked');
-        document.querySelector('.nav-list').classList.toggle('open');
-    });
-})
